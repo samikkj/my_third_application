86542d8f3607437b9d4abebd9ad35de764eba742
samikhafeez <samikhafeezkhan@gmail.com>
2022-09-05 16:39:07 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my_third_application
+ 
---
+++ flutter_application_4r5/lib/Views/home_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'This is my text 21',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: CircleAvatar(
+              radius: 150,
+              backgroundColor: Colors.amber,
+              child: Icon(
+                Icons.person,
+                size: 70,
+                color: Colors.teal,
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 200,
+                backgroundColor: Colors.blueGrey,
+                child: Icon(Icons.person, size: 30, color: Colors.black12),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
